5187873ab6efcf4db9acc06288bdc9bfcf8d2696
Vrindarak Vishwakarma <vrindarak7@gmail.com>
2025-04-03 10:00:44 +0530

    UI updated for event and news

---
+++ services/about.js
@@ -0,0 +1,4 @@
+document.getElementById("loadMoreBtn").addEventListener("click", function() {
+    document.getElementById("moreContent").style.display = "block"; // Show hidden content
+    document.getElementById("loadMoreBtn").style.display  = "none";
+});
