99e4de09ef1b51d713b42ff30b188fde22ba3708
Ramakrishnak <rkosanam@gmail.com>
2025-04-21 09:51:17 +0530

    content updated

--- services/about.js
+++ services/about.js
@@ -1,4 +1,20 @@
-document.getElementById("loadMoreBtn").addEventListener("click", function() {
-    document.getElementById("moreContent").style.display = "block"; // Show hidden content
-    document.getElementById("loadMoreBtn").style.display  = "none";
-});
+ 
+
+document.addEventListener("DOMContentLoaded", function () {
+    const readMoreButtons = document.querySelectorAll('.read-more');
+
+    readMoreButtons.forEach(function (btn) {
+      btn.addEventListener('click', function () {
+        const content = this.previousElementSibling;
+
+        if (content.style.display === 'none' || !content.style.display) {
+          content.style.display = 'block';
+          this.textContent = 'Read Less';
+        } else {
+          content.style.display = 'none';
+          this.textContent = 'Read More';
+        }
+      });
+    });
+  });
+
