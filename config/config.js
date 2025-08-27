c3f7da36b9eb5f56961226a398df2cefaf68777f
Bittu Kumar <bittusuvaidyam@outlook.com>
2025-08-27 18:15:08 +0530

    changes in lms

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 const ENV = {
-    API_BASE_URL: 'https://erp-ryss.ap.gov.in',
-    // API_BASE_URL: 'http://erp.localhost:8000',
+    // API_BASE_URL: 'https://erp-ryss.ap.gov.in',
+    API_BASE_URL: 'http://erp.localhost:8000',
 };
 
 export default ENV;
